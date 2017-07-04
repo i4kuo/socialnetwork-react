aad1bd877072b590c33ab9fafbb44341bbd83246
mathieupoterie <mathieupoterie@gmail.com>
2017-07-04 10:37:19 +0200

    test#4

--- src/LoggedInComponents/AppChild/Online.js
+++ src/LoggedInComponents/AppChild/Online.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from '../../axios';
-import NavBar from '../Siblings/navBar';
+import NavBar from '../Siblings/navbar';
 import Logo from '../../Logo';
 import {Link} from 'react-router';
 import {getSocket, UpdateUser} from '../../Socket'
